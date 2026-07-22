93512af2a22993d214d79e3de1ca1094d4408a2a
patryk <patryk@MacBook-Pro-patryk.local>
2026-07-22 13:47:05 +0200

    feat: change email

--- app/constants/common.ts
+++ app/constants/common.ts
@@ -1,4 +1,4 @@
 export const PHONE_NUMBER = "+48503758919";
 export const DISPLAYED_PHONE_NUMBER = "+48 503 758 919";
 export const APP_URL = "https://pdfwksef.pl/";
-export const EMAIL = "kontakt@pdfwksef.pl";
+export const EMAIL = "p.budnicki95@gmail.com";
